6026f1a4368c5b55ce94600699313eab40a8396f
Liya Kuncheva <liakuntcheva@icloud.com>
2025-09-09 18:01:49 +0300

    Second exercise from Data Types and Variables sheet completed

---
+++ 01_Basic/02_Data_Types_and_Variables/02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float a = 12.345f;
+        float b = 3456.091f;
+        double c = 34.567839023;
+        double d = 8923.1234857;
+
+        Console.WriteLine("Both float and double store fractional numbers.\n" +
+                          "Float is sufficient for storing 6 to 7 decimal digits.\n" +
+                          "Double is sufficient for storing 15 decimal digits.\n");
+
+        Console.WriteLine($"float a = {a}\n" +
+                          $"float b = {b}\n" +
+                          $"double c = {c}\n" +
+                          $"double d = {d}");
+    }
+}
