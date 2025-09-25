df7b30a436820a631577e0bdbba8250b4c910d22
Liya Kuncheva <liakuntcheva@icloud.com>
2025-09-25 15:31:18 +0300

    1-6 from Defining Classes Part 2 completed

---
+++ src/03_OOP_Principles/04_Defining_Classes/04.02/GenericList.cs
@@ -0,0 +1,151 @@
+﻿using System.Text;
+
+namespace _04._02
+{
+    public class GenericList<T>
+    {
+        private T[] elements;
+        private int count;
+        private const int DEFAULT_CAPACITY = 4;
+
+        public int Count
+        {
+            get { return this.count; }
+        }
+
+        public int Capacity
+        {
+            get { return this.elements.Length; }
+        }
+
+        public GenericList(int capacity = DEFAULT_CAPACITY)
+        {
+            if (capacity < 0)
+            {
+                throw new ArgumentOutOfRangeException("Capacity cannot be negative.");
+            }
+
+            this.elements = new T[capacity];
+            this.count = 0;
+        }
+
+        public void Add(T element)
+        {
+            if (this.count >= this.elements.Length)
+            {
+                this.Grow();
+            }
+
+            this.elements[this.count] = element;
+            this.count++;
+        }
+
+        public T Access(int index)
+        {
+            if (index < 0 || index >= this.count)
+            {
+                throw new IndexOutOfRangeException("Index is outside the valid range of the list.");
+            }
+
+            return this.elements[index];
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= this.count)
+            {
+                throw new IndexOutOfRangeException("Index is outside the valid range of the list.");
+            }
+
+            for (int i = index; i < this.count - 1; i++)
+            {
+                this.elements[i] = this.elements[i + 1];
+            }
+
+            this.elements[this.count - 1] = default(T);
+            this.count--;
+        }
+
+        public void InsertAt(T element, int index)
+        {
+            if (index < 0 || index > this.count)
+            {
+                throw new IndexOutOfRangeException("Index is outside the valid range for insertion.");
+            }
+
+            if (this.count >= this.elements.Length)
+            {
+                this.Grow();
+            }
+
+            for (int i = this.count; i > index; i--)
+            {
+                this.elements[i] = this.elements[i - 1];
+            }
+
+            this.elements[index] = element;
+            this.count++;
+        }
+
+        public void Clear()
+        {
+            this.count = 0;
+
+            for (int i = 0; i < this.elements.Length; i++)
+            {
+                this.elements[i] = default(T);
+            }
+        }
+
+        public int FindByValue(T value)
+        {
+            for (int i = 0; i < this.count; i++)
+            {
+                if (object.Equals(this.elements[i], value))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public override string ToString()
+        {
+            if (this.count == 0)
+            {
+                return "GenericList<T> is empty.";
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("List elements: [");
+
+            for (int i = 0; i < this.count; i++)
+            {
+                sb.Append(this.elements[i].ToString());
+                if (i < this.count - 1)
+                {
+                    sb.Append(", ");
+                }
+            }
+
+            sb.Append("]");
+
+            return sb.ToString();
+        }
+
+        private void Grow()
+        {
+            int newCapacity = this.elements.Length == 0 ? DEFAULT_CAPACITY : this.elements.Length * 2;
+
+            T[] newElements = new T[newCapacity];
+
+            Array.Copy(this.elements, newElements, this.count);
+
+            this.elements = newElements;
+
+            Console.WriteLine($"\n--> List grew! New capacity: {this.elements.Length}");
+        }
+    }
+}
---
+++ src/03_OOP_Principles/04_Defining_Classes/04.02/Point3D.cs
@@ -0,0 +1,56 @@
+﻿struct Point3D
+{
+    private double x;
+    private double y;
+    private double z;
+
+    private static readonly Point3D coordinateSystemStartPoint = new Point3D(0, 0, 0);
+
+    static Point3D CoordinateSystemStartPoint
+    {
+        get { return coordinateSystemStartPoint; }
+    }
+
+    public Point3D(double x, double y, double z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double X
+    {
+        get { return this.x; }
+        set { this.x = value; }
+    }
+
+    public double Y
+    {
+        get { return this.y; }
+        set { this.y = value; }
+    }
+
+    public double Z
+    {
+        get { return this.y; }
+        set { this.y = value; }
+    }
+
+    public override string ToString()
+    {
+        return $"Point coordinates: \nX: {X}\nY: {Y}\nZ: {z}";
+    }
+}
+
+internal record struct Point3D(int Item1, int Item2, int Item3)
+{
+    public static implicit operator (int, int, int)(Point3D value)
+    {
+        return (value.Item1, value.Item2, value.Item3);
+    }
+
+    public static implicit operator Point3D((int, int, int) value)
+    {
+        return new Point3D(value.Item1, value.Item2, value.Item3);
+    }
+}
