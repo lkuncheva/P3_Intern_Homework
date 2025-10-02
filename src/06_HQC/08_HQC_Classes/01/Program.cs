fa00c6950e550bd473e0306c9f668fe1d61d7026
Liya Kuncheva <liakuntcheva@icloud.com>
2025-10-02 18:09:04 +0300

    Ex 1-3 from 08_HQC_Classes completed

---
+++ src/06_HQC/08_HQC_Classes/01/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Abstraction;
+
+public class Program
+{
+    public static void Main()
+    {
+        try
+        {
+            Circle circle = new Circle(5);
+            Console.WriteLine($"Circle R={circle.Radius:f2}: " +
+                              $"Perimeter = {circle.CalculatePerimeter():f2}, " +
+                              $"Area = {circle.CalculateArea():f2}");
+
+            Rectangle rect = new Rectangle(2.5, 3.5);
+            Console.WriteLine($"Rectangle W={rect.Width:f2}, H={rect.Height:f2}: " +
+                              $"Perimeter = {rect.CalculatePerimeter():f2}, " +
+                              $"Area = {rect.CalculateArea():f2}");
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"[ERROR: Out of Range] {ex.Message}");
+            Console.ResetColor();
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"An unexpected fatal error occurred: {ex.Message}");
+            Console.ResetColor();
+
+            throw;
+        }
+    }
+}
