a800bfe2e480e8c3ef7b047d58c695fc5e85314a
Liya Kuncheva <liakuntcheva@icloud.com>
2025-10-02 12:14:35 +0300

    Adding extension classes

--- src/03_OOP_Principles/04_Defining_Classes/04.02/PathExtensions.cs
+++ src/03_OOP_Principles/04_Defining_Classes/04.02/PathExtensions.cs
@@ -1,18 +1,6 @@
 ﻿namespace _04._02;
-
-public static class Extensions
+public static class PathExtensions
 {
-    public static double CalculateDistance(Point3D p1, Point3D p2)
-    {
-        double deltaX = p2.X - p1.X;
-        double deltaY = p2.Y - p1.Y;
-        double deltaZ = p2.Z - p1.Z;
-
-        double distance = Math.Sqrt(Math.Pow(deltaX, 2) + Math.Pow(deltaY, 2) + Math.Pow(deltaZ, 2));
-
-        return distance;
-    }
-
     public static void SavePath(Path path, string filePath)
     {
         try
@@ -33,6 +21,11 @@ public static class Extensions
         {
             Console.WriteLine($"An unexpected error occurred: {ex.Message}");
         }
+    }
+
+    public static void SavePathTo(this Path path, string filePath)
+    {
+        SavePath(path, filePath);
     }
 
     public static Path LoadPath(string filePath)
@@ -70,5 +63,10 @@ public static class Extensions
         }
 
         return path;
+    }
+
+    public static void LoadPathTo(this string filePath)
+    {
+        LoadPath(filePath);
     }
 }
---
+++ src/03_OOP_Principles/04_Defining_Classes/04.02/Point3DExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace _04._02;
+
+public static class Point3DExtensions
+{
+    public static double CalculateDistance(Point3D p1, Point3D p2)
+    {
+        double deltaX = p2.X - p1.X;
+        double deltaY = p2.Y - p1.Y;
+        double deltaZ = p2.Z - p1.Z;
+
+        double distance = Math.Sqrt(Math.Pow(deltaX, 2) + Math.Pow(deltaY, 2) + Math.Pow(deltaZ, 2));
+
+        return distance;
+    }
+
+    public static double CalculateDistanceTo(this Point3D p1, Point3D p2)
+    {
+        return CalculateDistance(p1, p2);
+    }
+}
