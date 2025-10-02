e021e1ab299beca54b15a4605f4b327588284fee
Liya Kuncheva <liakuntcheva@icloud.com>
2025-10-02 15:28:17 +0300

    06_ControlFlows_Conditionals_Loops completed

---
+++ src/06_HQC/06_ControlFlow_Conditionals_Loops/02/Program.cs
@@ -0,0 +1,26 @@
+﻿// First code snippet
+var potato = GetPotato();
+
+if (potato != null&& potato.IsPeeled && !potato.IsRotten)
+{ 
+    Cook(potato);
+}
+
+// Second code snippet
+bool isWithinXBounds = x >= MIN_X && x <= MAX_X;
+bool isWithinYBounds = y >= MIN_Y && y <= MAX_Y;
+
+if (isWithinXBounds && isWithinYBounds && canVisitCell)
+{
+    VisitCell();
+}
+
+// Or this --->
+/*
+if (x >= MIN_X && x <= MAX_X &&
+    y >= MIN_Y && y <= MAX_Y &&
+    canVisitCell)
+{
+    VisitCell();
+}
+*/
