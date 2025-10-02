e2d0eef505c231a12dcf3356adfb57bf2e99824d
Liya Kuncheva <liakuntcheva@icloud.com>
2025-10-02 15:02:59 +0300

    05_Variables_Data_Constants completed

---
+++ src/06_HQC/05_Variables_Data_Constants/02/Program.cs
@@ -0,0 +1,44 @@
+﻿public class StatisticsCalculator
+{
+    public void PrintStatistics(double[] numbers)
+    {
+        if (numbers == null || !numbers.Any())
+        {
+            Console.WriteLine("Cannot calculate statistics for a null or empty array.");
+            return;
+        }
+
+        double maxElement = numbers[0];
+        double minElement = numbers[0];
+        double sum = 0;
+        double average;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] > maxElement)
+            {
+                maxElement = numbers[i];
+            }
+
+            if (numbers[i] < minElement)
+            {
+                minElement = numbers[i];
+            }
+
+            sum += numbers[i];
+        }
+
+        average = sum / numbers.Length;
+
+        // Or we can just do this
+        /*
+        double maxElement = numbers.Max();
+        double minElement = numbers.Min();
+        double average = numbers.Average();
+        */
+
+        Console.WriteLine($"Max: {maxElement}");
+        Console.WriteLine($"Min: {minElement}");
+        Console.WriteLine($"Average: {average}");
+    }
+}
