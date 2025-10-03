96e9c770e8f2703a8846e71863d3d00c12720ecf
Liya Kuncheva <liakuntcheva@icloud.com>
2025-10-03 11:10:31 +0300

    08_HQC_Classes completed

---
+++ src/06_HQC/08_HQC_Classes/04/LocalCourse.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+namespace InheritanceAndPolymorphism;
+
+public class LocalCourse : Course
+{
+    private string? lab;
+
+    public string? Lab
+    {
+        get => this.lab;
+        set
+        {
+            if (value != null && string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Lab name cannot be set to an empty or whitespace string.", nameof(Lab));
+            }
+
+            this.lab = value;
+        }
+    }
+
+    public LocalCourse(string name) : base(name) { }
+
+    public LocalCourse(string name, string? teacherName) : base(name, teacherName) { }
+
+    public LocalCourse(string name, string? teacherName, IList<string> students, string? lab = null)
+            : base(name, teacherName, students)
+    {
+        Lab = lab;
+    }
+
+    public override string ToString()
+    {
+        StringBuilder result = new StringBuilder("LocalCourse {");
+        result.Append(GetBaseCourseInfo());
+
+        if (!string.IsNullOrWhiteSpace(Lab))
+        {
+            result.Append($"; Lab = {Lab}");
+        }
+
+        result.Append(" }");
+
+        return result.ToString();
+    }
+}
